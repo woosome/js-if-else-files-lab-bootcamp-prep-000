cdf958dfab434970d374c29c0c554fa9590f030e
Sun Woo <sunhawoo@gmail.com>
2018-01-14 19:32:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var height = "74"
 
 // Don't worry about this
 module.exports = { name, height
-  if(name == "Joe"){
-    console.log("The name is incorrect");
-  }else{
-    
-  }
 }
+
+if(name =="Joe"){
+  console.log("The name is correct");
+}else{
+  console.log("The name is incorrect");
+}
+
+
 
